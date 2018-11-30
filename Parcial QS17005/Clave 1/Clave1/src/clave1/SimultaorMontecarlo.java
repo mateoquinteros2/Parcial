3cc9d893ed7cec4dc56b2c268335662b4fdb5ad0
mateoquinteros2 <mateoquinteros2@gmail.com>
2018-11-30 02:10:19 -0600

    Update SimultaorMontecarlo.java

--- Parcial QS17005/Clave 1/Clave1/src/clave1/SimultaorMontecarlo.java
+++ Parcial QS17005/Clave 1/Clave1/src/clave1/SimultaorMontecarlo.java
@@ -16,20 +16,26 @@ public class SimultaorMontecarlo {
     
     
     public void MonteCarlos(){
+        
 Scanner entrada = new Scanner(System.in); 
-   Atributtos stdin = new Atributtos();  
- 
-System.out.print("Escriba una semilla: ");
+   Atributtos stdin = new Atributtos();  //instancia de la clse atributos
+        
+ //procedimiento de captura de datos para la realizacion del algoritmo
+System.out.print("Inserte una semilla: ");
 stdin.setSemilla(entrada.nextInt());
-System.out.print("Escriba una constante multiplicativa: ");
+        
+System.out.print("Inserte una constante multiplicativa: ");
  stdin.setCmultiplicativa(entrada.nextInt());
-System.out.print("Escriba el módulo: ");
+        
+System.out.print("Inserte el módulo: ");
 stdin.setModulo(entrada.nextInt());
 
+//Una vez capturado los datos se procede a realizar el algoritmo        
 for (stdin.i=1; stdin.i<=20; stdin.i++){
      stdin.numero = (stdin.getCmultiplicativa()*stdin.getSemilla()) % stdin.getModulo();   
      stdin.numero2 = (int) ((double)stdin.numero / (double)(stdin.getModulo()-1));
    System.out.printf("%d. %d (%.4f)\n", stdin.i ,stdin.numero ,stdin.numero2 ); 
+    
    stdin.semilla = stdin.numero; 
 }
     }
